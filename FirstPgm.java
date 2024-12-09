f6d80b678de02ab694826be159521ecbd326c6a8
veelinktech <104140334+veelinktech@users.noreply.github.com>
2024-12-09 11:17:36 +0530

    Second_commit
    adding new logics subtraction, multiplication and division.
    Finally modify the values in add logic

--- FirstPgm.java
+++ FirstPgm.java
@@ -4,7 +4,10 @@ public class Sample
 public static void main(String[] args)
 {
     System.out.println("Hi");
-    System.out.println(5+5);
+    System.out.println(5+10);
+    System.out.println(5*10);
+    System.out.println(5-10);
+    System.out.println(5/10)
 
 }
     
