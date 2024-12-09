7cb1832fecb8b998edc89bcc98c85a1c7ae42996
lavanyaalai41 <lavanyaalai41@gmail.com>
2024-12-09 11:31:26 +0530

    Updated by lavanya
    i am adding subtraction

--- FirstPgm.java
+++ FirstPgm.java
@@ -4,16 +4,20 @@ public class Sample
     {
          System.out.println(5+10);
     }
-
-public static void main(String[] args)
+ public static void sub()
+    {
+         System.out.println(5-10);
+    }
+    public static void main(String[] args)
 {
     System.out.println("Hi");
     add();
+    sub();
     System.out.println(5*10);
-    System.out.println(5-10);
     System.out.println(5/10)
 
 }
     
 }
+
 
