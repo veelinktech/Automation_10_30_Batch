447a880350c9716efb01c86a7df0087818a6ee7a
kowshick12B <kowshick12@gmail.com>
2024-12-09 11:24:01 +0530

    kowshick 1st commit
    addded new method ,add

--- FirstPgm.java
+++ FirstPgm.java
@@ -1,10 +1,14 @@
 public class Sample
 {
+    public static void add()
+    {
+         System.out.println(5+10);
+    }
 
 public static void main(String[] args)
 {
     System.out.println("Hi");
-    System.out.println(5+10);
+    add();
     System.out.println(5*10);
     System.out.println(5-10);
     System.out.println(5/10)
